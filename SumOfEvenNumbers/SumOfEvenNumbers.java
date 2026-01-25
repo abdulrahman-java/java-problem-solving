d3841640ca3faa2e50ebb983c52469d80eb41143
abdulrahman-java <abdulrahman.dev.java@gmail.com>
2026-01-25 23:59:49 +0300

    Implement sum of even numbers from user input

--- SumOfEvenNumbers/SumOfEvenNumbers.java
+++ SumOfEvenNumbers/SumOfEvenNumbers.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int first = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= first; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+        scanner.close();
+    }
+}
 
