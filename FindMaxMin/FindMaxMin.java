765f290b2fcc8e7e504192fd0866778e805db3c4
abdulrahman-java <abdulrahman.dev.java@gmail.com>
2026-02-02 01:50:26 +0300

    Add max and min element problem solution

---
+++ FindMaxMin/FindMaxMin.java
@@ -0,0 +1,22 @@
+public class FindMaxMin {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 9, 2, 7, 1};
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
+}
